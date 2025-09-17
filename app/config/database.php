128a041967c21a87eb43b06e26956cc2d9488e13
John Lexter Decillo <johnlexterdecillo5@gmail.com>
2025-09-17 19:20:00 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12798070',
-    'password'	=> 'dLeUAahfaD',
-    'database'	=> 'sql12798070',
+    'username'	=> 'sql12798982',
+    'password'	=> 'Lb4gkZfpjN',
+    'database'	=> 'sql12798982',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
